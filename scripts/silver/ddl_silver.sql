b21ba62f45ee30c85ef97d307fee7f7392bfe7b8
Maxim-Karmanov <70822880+Maxim-Karmanov@users.noreply.github.com>
2025-12-05 12:41:19 +0500

    Fix typo in column name from 'maintence' to 'maintenance'

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -89,6 +89,6 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
 	id NVARCHAR(50),
 	cat NVARCHAR(50),
 	subcat NVARCHAR(50),
-	maintence NVARCHAR(50),
+	maintenance NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
